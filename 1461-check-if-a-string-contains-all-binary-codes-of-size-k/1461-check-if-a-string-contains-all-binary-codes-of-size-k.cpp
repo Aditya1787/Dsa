66a270ad12cd4645b05bcc13b08c19b1fddcdb8a
Aditya Kumar Mishra <148345694+Aditya1787@users.noreply.github.com>
2026-02-23 21:49:16 +0530

    Time: 8 ms (97.28%), Space: 21.9 MB (99.28%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,15 +4,12 @@ class Solution {
     const int n = 1 << k;
     if (s.length() < n)
       return false;
-
-    // used[i] := true if i is a substring of `s`
     vector<bool> used(n);
 
     int window = k == 1 ? 0 : stoi(s.substr(0, k - 1), nullptr, 2);
     for (int i = k - 1; i < s.length(); ++i) {
-      // Include the s[i].
+      
       window = (window << 1) + (s[i] - '0');
-      // Discard the s[i - k].
       window &= n - 1;
       used[window] = true;
     }
